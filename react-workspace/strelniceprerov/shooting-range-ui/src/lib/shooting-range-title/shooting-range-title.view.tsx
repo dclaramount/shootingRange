e252be846a8dd860604c314800b50d323214c67d
Diego Claramount <dieg.ruiz@praguelabs.com>
2024-03-02 15:14:11 +0100

    fix: react-clock

--- react-workspace/strelniceprerov/shooting-range-ui/src/lib/shooting-range-title/shooting-range-title.view.tsx
+++ react-workspace/strelniceprerov/shooting-range-ui/src/lib/shooting-range-title/shooting-range-title.view.tsx
@@ -1,6 +1,9 @@
 import { styled } from '@mui/material';
 import { ShootingRangeTitleProps } from './shooting-range-title';
 import { StyledBaseProps } from '../common/types';
+import Clock from 'react-live-clock';
+import React from 'react';
+
 
 interface ShootingRangeTitleViewProps
   extends ShootingRangeTitleProps,
@@ -11,9 +14,8 @@ interface ShootingRangeTitleViewProps
 export const ShootingRangeTitleView = styled(
   (props: ShootingRangeTitleViewProps) => (
     <div className={props.className}>
-      <h1>
-        This is the place holder
-      </h1>
+      <h1>This is the place holder</h1>
+      <Clock format={'HH:mm:ss'} ticking={true} timezone={'US/Pacific'} />
     </div>
   )
 )``;
