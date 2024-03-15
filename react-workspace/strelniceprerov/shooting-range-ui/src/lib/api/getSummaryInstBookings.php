63b0d3f0191ad7237436e2ebc668db6a131b1e5c
Diego Claramount <dieg.ruiz@praguelabs.com>
2024-03-15 02:15:19 +0100

    api calls for summary views

---
+++ react-workspace/strelniceprerov/shooting-range-ui/src/lib/api/getSummaryInstBookings.php
@@ -0,0 +1,39 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST");
+header('Content-Type: application/json; charset=utf-8');
+
+$databaseHost = 'localhost';
+$databaseUsername = 'www-strelnic';
+$databasePassword = 'bQASvDoM9K4g';
+$databaseName = 'www-strelnic';
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
+
+
+//Variable that will hold the response
+$responseArray = array();
+
+//Executing the multi query
+$query = "SELECT  sBookingsI.OccupancyBooked  as 'OccupancyBooked',  
+                  sBookingsI.MaxOccupancy    as 'MaxOccupancy',
+                  sBookingsI.SegmentStarts   as 'SegmentStarts',
+                  sBookingsI.SegmentEnds     as 'SegmentEnd'
+                  FROM  Summary_Instructor_Booking_Segments as sBookingsI;";
+ 
+//Retrieving the records
+$res = mysqli_query($mysqli, $query, MYSQLI_USE_RESULT) or die( mysqli_error($mysqli));
+if ($res) {
+  $index = 1;
+  while ($row = mysqli_fetch_row($res)) {
+    $responseArray[]=array( 'occupancyBooked'                 => $row[0],
+                            'maxOccupancy'                    => $row[1],
+                            'segmentStarts'                   => $row[2],
+                            'segmentEnd'                      => $row[3]
+                          );
+    $index++;
+  }
+  $res->free_result();
+}
+mysqli_close($mysqli);
+echo json_encode($responseArray);
+?>
