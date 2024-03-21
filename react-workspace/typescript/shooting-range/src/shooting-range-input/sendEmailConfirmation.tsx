4b8d08e25f5352e59436536dda1c583bcb1a5426
Diego Claramount <dieg.ruiz@praguelabs.com>
2024-03-21 06:50:21 +0100

    fix: draft function to send email remove secret for real

---
+++ react-workspace/typescript/shooting-range/src/shooting-range-input/sendEmailConfirmation.tsx
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+export function SendEmail(){
+  axios.post(
+                            'https://api.sendgrid.com/v3/mail/send', 
+                            '{"personalizations": [{"to": [{"email": "diegoaclaramount@gmail.com"}]}],"from": {"email": "diegoantonio.claramountruiz@student.upce.cz"},"subject": "Sending with SendGrid is Fun","content": [{"type": "text/plain", "value": "and easy to do anywhere, even with cURL"}]}', 
+                            {
+                              headers: {
+                                'Authorization': `Bearer PLACEHOLDER`,
+                                'Content-Type': 'application/json'
+                                }
+                            }
+                            ).then((res) => {
+                              console.log(res);
+                            })
+                            .catch((err) => {
+                              console.log(err);
+                            })
+  
+}
