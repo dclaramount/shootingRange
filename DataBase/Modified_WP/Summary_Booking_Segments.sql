d7fe58e44ff8057293a5597f1e80f8de4296e22a
Diego Claramount <dieg.ruiz@praguelabs.com>
2024-04-10 12:07:30 +0200

    fix: update summary booking segments

--- DataBase/Modified_WP/Summary_Booking_Segments.sql
+++ DataBase/Modified_WP/Summary_Booking_Segments.sql
@@ -1,16 +1,20 @@
 DROP VIEW IF EXISTS Summary_Booking_Segments;
 CREATE VIEW `Summary_Booking_Segments` AS
 SELECT
-    iBookingSegment.location_id				as 'Location_Id',
-    location.name							as 'Location_Name',
+    location.id                             as 'location_id',
+    location.name                           as 'location_name',
+    iBookingSegment.location_id				as 'Service_Id',
+    services.name							as 'Service_Name',
     SUM(iBookingSegment.number_of_people) 	as 'OccupancyBooked',
-    location.capacity						as 'Max_Occupancy',
+    services.max_capacity				    as 'Max_Occupancy',
     iBookingSegment.start_time        		as 'SegmentStarts',
     iBookingSegment.end_time  				as 'SegmentEnds',
     SUM(iBookingSegment.with_instructor)	as 'InstructorsBooked'
 FROM  `invoice_item` as iBookingSegment
-          INNER JOIN `location`
-                     ON iBookingSegment.location_id = location.id
+          INNER JOIN `services`
+                     ON iBookingSegment.location_id = services.id
+            INNER JOIN `location`
+                    ON location.id = services.id
 WHERE iBookingSegment.isDeleted = 0
 GROUP BY iBookingSegment.start_time, iBookingSegment.location_id
 ORDER BY iBookingSegment.start_time ASC;
