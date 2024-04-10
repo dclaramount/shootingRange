8ff7a4c7f4e13f406c8b36111b21b002b1ce62f1
Diego Claramount <dieg.ruiz@praguelabs.com>
2024-04-10 12:07:15 +0200

    fix: update of the endpoints for getting the list of bookings

--- react-workspace/strelniceprerov/shooting-range-ui/src/lib/api/getListShootingRange.php
+++ react-workspace/strelniceprerov/shooting-range-ui/src/lib/api/getListShootingRange.php
@@ -8,25 +8,39 @@ $databaseUsername = 'www-strelnic';
 $databasePassword = 'bQASvDoM9K4g';
 $databaseName = 'www-strelnic';
 $mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
-
+$mysqli->set_charset("utf8mb4");
 
 //Variable that will hold the response
 $responseArray = array();
 
 //Executing the multi query
-$query = "SELECT * FROM location";
+$query = "SELECT  serv.id           as 'Id',
+                  serv.location_id  as 'LocationId',
+                  location.name     as 'LocationName',
+                  serv.name         as 'Name',
+                  serv.max_capacity as 'Capacity',
+                  serv.isDeleted    as  'isDeleted',
+                  serv.userId       as  'userId',
+                  serv.created      as  'created',
+                  serv.updated      as  'updated'
+                  FROM services     as  serv
+                    INNER JOIN location
+                        ON serv.location_id = location.id 
+                  WHERE serv.isDeleted=0;";
  
 //Retrieving the records
 $res = mysqli_query($mysqli, $query, MYSQLI_USE_RESULT) or die( mysqli_error($mysqli));
 if ($res) {
   while ($row = mysqli_fetch_row($res)) {
-    $responseArray[]=array( 'id'        => $row[0], 
-                            'name'      => $row[1],
-                            'capacity'  => $row[2],
-                            'isDeleted' => $row[3],
-                            'userId'    => $row[4],
-                            'created'   => $row[5],
-                            'updated'   => $row[6]);
+    $responseArray[]=array( 'id'            => $row[0], 
+                            'locationId'    => $row[1],
+                            'locationName'  => $row[2],
+                            'serviceName'   => $row[3],
+                            'capacity'      => $row[4],
+                            'isDeleted'     => $row[5],
+                            'userId'        => $row[6],
+                            'created'       => $row[7],
+                            'updated'       => $row[8]);
   }
   $res->free_result();
 }
--- react-workspace/strelniceprerov/shooting-range-ui/src/lib/api/getSummaryBookings.php
+++ react-workspace/strelniceprerov/shooting-range-ui/src/lib/api/getSummaryBookings.php
@@ -8,18 +8,20 @@ $databaseUsername = 'www-strelnic';
 $databasePassword = 'bQASvDoM9K4g';
 $databaseName = 'www-strelnic';
 $mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
-
+$mysqli->set_charset("utf8mb4");
 
 //Variable that will hold the response
 $responseArray = array();
 
 //Executing the multi query
-$query = "SELECT  sBookings.Location_Id     as 'LocationId',
-                  sBookings.Location_Name   as 'Location',
-                  sBookings.OccupancyBooked as 'OccupancyBooked',
-                  sBookings.Max_Occupancy    as 'MaxOccupancy',
-                  sBookings.SegmentStarts   as 'SegmentStarts',
-                  sBookings.SegmentEnds     as 'SegmentEnd'
+$query = "SELECT  sBookings.Location_Id       as  'LocationId',
+                  sBookings.Location_Name     as  'Location',
+                  sBookings.Service_Id        as  'ServiceId',
+                  sBookings.Service_Name      as  'ServiceName',
+                  sBookings.OccupancyBooked   as  'OccupancyBooked',
+                  sBookings.Max_Occupancy     as  'MaxOccupancy',
+                  sBookings.SegmentStarts     as  'SegmentStarts',
+                  sBookings.SegmentEnds       as  'SegmentEnd'
                   FROM  Summary_Booking_Segments as sBookings;";
  
 //Retrieving the records
@@ -30,10 +32,12 @@ if ($res) {
     $responseArray[]=array( 'id'                              => $index,
                             'locationId'                      => $row[0],           
                             'location'                        => $row[1],
-                            'occupancyBooked'                 => $row[2],
-                            'maxOccupancy'                    => $row[3],
-                            'segmentStarts'                   => $row[4],
-                            'segmentEnd'                      => $row[5]
+                            'serviceId'                       => $row[2],
+                            'service'                         => $row[3],
+                            'occupancyBooked'                 => $row[4],
+                            'maxOccupancy'                    => $row[5],
+                            'segmentStarts'                   => $row[6],
+                            'segmentEnd'                      => $row[7]
                           );
     $index++;
   }
