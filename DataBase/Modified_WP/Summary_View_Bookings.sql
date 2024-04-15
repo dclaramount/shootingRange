3146f1cb4eeda0e8e282e5477f05a6833e11dd38
Diego Claramount <dieg.ruiz@praguelabs.com>
2024-04-15 21:35:13 +0200

    fix: updating summary view bookings view

--- DataBase/Modified_WP/Summary_View_Bookings.sql
+++ DataBase/Modified_WP/Summary_View_Bookings.sql
@@ -4,27 +4,28 @@ SELECT
     iItem.id                as 'id',
     iItem.invoice_id        as 'invoiceId',
     invType.type            as  'invoiceType',
+    serv.location_id        as 'locationId',
     loc.name                as 'locationName',
+    serv.id                 as  'serviceId',
+    serv.name               as  'serviceName',
     iItem.number_of_people  as 'occupancy',
-    loc.capacity            as 'locationMaxOccupancy',
+    serv.max_capacity       as 'locationMaxOccupancy',
     iItem.number_of_hours   as 'length',
     iItem.with_instructor  as 'instructor',
     iItem.start_time        as 'startTime',
     iItem.end_time          as  'endTime',
-    inv.is_deleted          as 'isDeleted',
     uInfo.name              as  'customerName',
     uInfo.email             as  'customerEmail',
-    uInfo.phoneNumber       as  'customerPhoneNumber',
-    inv.created             as  'createdOn',
-    inv.updated             as  'lastModified',
-    inv.parent_invoice      as  'parentInvoice'
+    uInfo.phoneNumber       as  'customerPhoneNumber'
 FROM invoice_item as iItem
-         INNER JOIN `location` loc
-                    ON iItem.location_id = loc.id
-         INNER JOIN `invoice` inv
-                    ON iItem.invoice_id = inv.id
-         INNER JOIN `invoice_type` invType
-                    ON inv.invoice_type_id = invType.id
-         INNER JOIN `user_list` uInfo
-                    ON uInfo.id = inv.user_id
+    INNER JOIN `services` serv
+                    ON iItem.location_id = serv.id
+    INNER JOIN `location` loc
+               ON serv.location_id = loc.id
+    INNER JOIN `invoice` inv
+               ON iItem.invoice_id = inv.id
+    INNER JOIN `invoice_type` invType
+               ON inv.invoice_type_id = invType.id
+    INNER JOIN `user_list` uInfo
+               ON uInfo.id = inv.user_id
 ORDER BY iItem.invoice_id ASC, iItem.created ASC;
