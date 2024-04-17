3c8ccff35221872aa37cc233ba50c246d18fd9b6
Diego Claramount <dieg.ruiz@praguelabs.com>
2024-04-17 18:25:04 +0200

    fix: popup table for booking management.

--- react-workspace/strelniceprerov/shooting-range-ui/src/lib/shooting-range-input/BookingManagement/Dashboard/Components/BookingsViewer/ManagementPopUp.tsx
+++ react-workspace/strelniceprerov/shooting-range-ui/src/lib/shooting-range-input/BookingManagement/Dashboard/Components/BookingsViewer/ManagementPopUp.tsx
@@ -1,29 +1,49 @@
 import React from 'react';
 import { ManagementDashboardContext } from '../../../../components/Context/ManagementDashboardContext';
 import {format } from 'date-fns';
+import { EditRowTable } from './EditRowTable';
 
+const tableStyle : React.CSSProperties = { border: "1px solid black", borderCollapse: "collapse", borderRadius:'10px', marginTop:'15px', marginBottom:'15px'};
+const headerCellStyle: React.CSSProperties  = { 
+  border: "1px solid black", 
+  borderCollapse: "collapse", 
+  fontFamily:'sans-serif', 
+  fontWeight:'bolder', 
+  fontSize:'13px', 
+  fontStyle:'italic', 
+  padding:'5px',
+  textAlign:'center' 
+}
 //This Renders the PopUp that will navigate the user throughout the booking confirmation process.
 export function ManagementPopUp({closeModalFunction} : any) {
-  interface UserEntry {
-    ID: number;
-    id: string;
-    name: string;
-    email: string;
-    phoneNumber: string;
-    shootingPermit?: string;
-    updatedOn: Date;
-    userId: string;
-  }
 
   const {selectedSegment, selectedLocation, allInvoices} = React.useContext(ManagementDashboardContext);
-  const filtered = allInvoices.filter((sb : any) => (format(new Date(sb.startTime * 1000), 'yyyy-MM-d HH:mm:ss')===selectedSegment));
+  const filtered = allInvoices.filter((sb : any) => (format(new Date(sb.startTime * 1000), 'yyyy-MM-d HH:mm')===selectedSegment[0]) && (parseInt(sb.serviceId)===parseInt(selectedLocation)));
   return(
     <div>
-      <div style={{backgroundColor:'white', maxWidth:'500px', padding:'50px', border:'1px solid #e7e7e9', borderRadius:'15px', outline:'20px solid #fafafa'}}>
-        <a className="close" onClick={closeModalFunction} style={{marginRight:'10px', marginTop:'10px', width:'24px', height:'24px', cursor:'pointer'}}>
-            <i className="fa fa-times"></i>
-        </a>
-        {filtered.map((inv : any)=>{return (<div>Diego</div>)})}
+      <div style={{backgroundColor:'white', width:'1250px', padding:'5px', border:'1px solid #e7e7e9', borderRadius:'15px', outline:'20px solid #fafafa', display:'flex', flexDirection:'column'}}>
+        <div className="close" style={{marginLeft:'98%', marginRight:'auto', width:'24px', height:'24px', cursor:'pointer'}} onClick={closeModalFunction}>
+          <i className="fa fa-times"></i>
+        </div>
+        <table id="outputTable" style={tableStyle}> 
+        <caption style={{captionSide:'top', marginBottom:'15px', marginLeft:'50%', fontSize:'20px', fontWeight:'bolder'}}>Summary</caption>
+        <tr> 
+            <th style={headerCellStyle} >Invoice Id</th> 
+            <th style={headerCellStyle}>Invoice Type</th> 
+            <th style={headerCellStyle}>Střelnice</th> 
+            <th style={headerCellStyle}>Datum a čas</th> 
+            <th style={headerCellStyle}>Délka rezervace(h)</th>
+            <th style={headerCellStyle}>Zbrojní průkaz</th>
+            <th style={headerCellStyle}>Instruktor</th>
+            <th style={headerCellStyle}>Jméno a příjmení</th> 
+            <th style={headerCellStyle}>E-mail</th> 
+            <th style={headerCellStyle}>Telefon</th>
+            <th></th> 
+        </tr>
+        {filtered.map((invoiceInfo : any)=>{
+          return (<EditRowTable inv={invoiceInfo}/>)
+        })}
+    </table> 
       </div>
     </div>
 )}
---
+++ react-workspace/typescript/shooting-range/src/shooting-range-input/BookingManagement/Dashboard/Components/BookingsViewer/EditRowTable.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { ManagementDashboardContext } from '../../../../Context/ManagementDashboardContext';
+import {format } from 'date-fns';
+
+const tableCell: React.CSSProperties  = { 
+  border: "1px solid black", 
+  borderCollapse: "collapse", 
+  fontFamily:'sans-serif', 
+  fontSize:'10px',
+  padding:'5px', 
+  textAlign:'center'
+}
+const iconsCell: React.CSSProperties = {
+  display: "flex",
+  borderTop: "1px solid black", 
+  borderCollapse: "collapse",
+  padding:'7.5px', 
+}
+const iconStyle: React.CSSProperties = {
+  display: "flex",
+  margin:'5px', 
+}
+//This Renders the PopUp that will navigate the user throughout the booking confirmation process.
+export function EditRowTable({inv} : any) {
+  const [edit, setEdit] = React.useState(false);
+
+  return(
+          <tr id={inv.invoiceId}>
+            <td style={tableCell}>{inv.invoiceId}</td>
+            <td style={tableCell}>{inv.invoiceType}</td>
+            <td style={tableCell}>{inv.serviceName}</td>
+            <td style={tableCell}>{format(new Date(inv.startTime * 1000), 'd.MM.yyyy HH:mm:ss')}</td>
+            <td style={tableCell}>{inv.lenght}</td>
+            <td style={tableCell}>PlaceHolder</td>
+            <td style={tableCell}><input type="checkbox" id="scales" name="scales" checked={inv.instructor}/></td>
+            <td style={tableCell}>{inv.customerName}</td>
+            <td style={tableCell}>{inv.customerEmail}</td>
+            <td style={tableCell}>+{inv.phoneNumber}</td>
+            <td style={iconsCell}>
+              {!edit && <i style={iconStyle} className="fas fa-edit" onClick={() => setEdit(true)}></i>}
+              {!edit && <i style={iconStyle} className="fa fa-trash" aria-hidden="true"></i>}
+              {edit && <i  style={iconStyle} className="fas fa-save"></i>}
+              {edit && <i  style={iconStyle} className="fa fa-times" onClick={() => setEdit(false)} aria-hidden="true"></i>}
+            </td>
+          </tr>
+        )
+ }
