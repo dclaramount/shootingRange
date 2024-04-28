34b987179a429b5c2bd9eea066cd7e769728b005
Diego Claramount <dieg.ruiz@praguelabs.com>
2024-04-28 19:29:45 -0400

    fix: cleaning unused code

--- react-workspace/strelniceprerov/shooting-range-ui/src/lib/shooting-range-input/BookingManagement/BookingManagementSpace.tsx
+++ react-workspace/strelniceprerov/shooting-range-ui/src/lib/shooting-range-input/BookingManagement/BookingManagementSpace.tsx
@@ -1,9 +1,7 @@
 import * as React from 'react';
-import { ManagementDashboardContext } from '../components/Context/ManagementDashboardContext';
 import { ManagementDashboard } from './Dashboard/ManagementDashboard';
 /* Locally Used Constants*/
 export function BookingManagementSpace() {
-  const {summaryBookingSegments} = React.useContext(ManagementDashboardContext);
   return (
     <div>
       <ManagementDashboard/>
