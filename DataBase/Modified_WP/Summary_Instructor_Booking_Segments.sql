820c8b797a1ab3734fe1f34299cf4857aefe0edc
Diego Claramount <dieg.ruiz@praguelabs.com>
2024-06-03 21:26:52 +0200

    fix: calcualtion of instructors capacity booked

--- DataBase/Modified_WP/Summary_Instructor_Booking_Segments.sql
+++ DataBase/Modified_WP/Summary_Instructor_Booking_Segments.sql
@@ -3,9 +3,9 @@ CREATE VIEW `Summary_Instructor_Booking_Segments` AS
 SELECT
     sISegments.SegmentStarts					as 'SegmentStarts',
     sISegments.SegmentEnds						as 'SegmentEnds',
-    IFNULL(sBSegments.OccupancyBooked,0)		as 'OccupancyBooked',
+    IFNULL(sBSegments.InstructorsBooked,0)		as 'OccupancyBooked',
     sISegments.AvailableInstructors				as 'MaxOccupancy'
 FROM  `Summary_Instructor_Segments` as sISegments
           LEFT JOIN `Summary_Booking_Segments` sBSegments
-                    ON sISegments.SegmentStarts = sBSegments.SegmentStarts AND sBSegments.InstructorsBooked > 1
+                    ON sISegments.SegmentStarts = sBSegments.SegmentStarts AND sBSegments.InstructorsBooked >= 1
 ORDER BY sISegments.SegmentStarts ASC;
