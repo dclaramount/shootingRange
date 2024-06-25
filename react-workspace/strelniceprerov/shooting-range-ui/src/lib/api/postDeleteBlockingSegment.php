d102edda679ac3f9b346674adff53971e7e65504
Diego Claramount <dieg.ruiz@praguelabs.com>
2024-06-25 15:40:28 +0200

    fix: endpoint delete blocking segment.

---
+++ react-workspace/strelniceprerov/shooting-range-ui/src/lib/api/postDeleteBlockingSegment.php
@@ -0,0 +1,91 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST");
+header('Content-Type: application/json; charset=utf-8');
+/*------------------------------------------------------------------------------------------------------------*/
+/*                                                    Starting Variables                                      */
+/*------------------------------------------------------------------------------------------------------------*/
+//General variables to connect to DB
+$databaseHost                                         =   'localhost';
+$databaseUsername                                     =   'www-strelnic';
+$databasePassword                                     =   'bQASvDoM9K4g';
+$databaseName                                         =   'www-strelnic';
+$dateTimeActual                                       =   new DateTime();
+$mysqli                                               =   mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
+//HTTP-Codes that will return
+$GOOD_REQUEST                                         =   200;
+$BAD_REQUEST                                          =   400;
+$CONFLICT                                             =   409;
+$INTERNAL_SERVER_ERROR                                =   500;
+//Initialization of the Variables for this End-Point
+$requestResponse                                      =   $GOOD_REQUEST;
+$responseMessage                                      =   "";
+//Retrieving the Messages for the differnt HTTP-Codes of this endpoint TODO://
+$query                                                =   "SELECT value FROM `global_variables` WHERE name='200_postCreateBlockingSegment';";
+$res                                                  =   mysqli_query($mysqli, $query, MYSQLI_USE_RESULT) or die( mysqli_error($mysqli));
+while ($row = mysqli_fetch_row($res)) {
+  $resultMessage[$GOOD_REQUEST]                       =   $row[0];
+}
+$query                                                =   "SELECT value FROM `global_variables` WHERE name='400_postCreateBlockingSegment';";
+$res                                                  =   mysqli_query($mysqli, $query, MYSQLI_USE_RESULT) or die( mysqli_error($mysqli));
+while ($row = mysqli_fetch_row($res)) {
+  $resultMessage[$BAD_REQUEST]                        =   $row[0];
+}
+$query                                                =   "SELECT value FROM `global_variables` WHERE name='409_postCreateBlockingSegment';";
+$res                                                  =   mysqli_query($mysqli, $query, MYSQLI_USE_RESULT) or die( mysqli_error($mysqli));
+while ($row = mysqli_fetch_row($res)) {
+  $resultMessage[$CONFLICT]                           =   $row[0];
+}
+$query                                                =   "SELECT value FROM `global_variables` WHERE name='500_postCreateBlockingSegment';";
+$res                                                  =   mysqli_query($mysqli, $query, MYSQLI_USE_RESULT) or die( mysqli_error($mysqli));
+while ($row = mysqli_fetch_row($res)) {
+  $resultMessage[$INTERNAL_SERVER_ERROR]              =   $row[0];
+}
+/*------------------------------------------------------------------------------------------------------------*/
+/*                                            Destructuring Query Parameters                                  */
+/*------------------------------------------------------------------------------------------------------------*/
+try{
+  if (isset($_GET['guid']) && $_GET['guid']!="") {
+    $guid                                             =   $_GET['guid'];
+  } else {
+    $guid                                             =   "";
+  }
+  /*------------------------------------------------------------------------------------------------------------*/
+  /*                    Verification if Start Segment is older than actual time                                 */  
+  /*------------------------------------------------------------------------------------------------------------*/
+  $start                                              =   new DateTime($startSegment);
+  $end                                                =   new DateTime($endSegment);
+  $startSegmentIsInThePast                            =   $start <=  $dateTimeActual;
+  if($startSegmentIsInThePast){
+    $requestResponse                                  =   $CONFLICT;
+    $responseMessage                                  =   $resultMessage[$CONFLICT];
+  };
+  /*------------------------------------------------------------------------------------------------------------*/
+  /*                                  Change the Flag of the segment to Deleted it                              */  
+  /*------------------------------------------------------------------------------------------------------------*/
+  $query = "UPDATE blocking_segments SET isDeleted=1 WHERE guid LIKE '%$guid%';";
+  $res = mysqli_query($mysqli, $query, MYSQLI_USE_RESULT) or die( mysqli_error($mysqli));
+  mysqli_close($mysqli);
+  http_response_code($GOOD_REQUEST);
+} catch (Exception $e){
+  /*------------------------------------------------------------------------------------------------------------*/
+  /*                                  Catch Loop for 500-Internal Server Error                                  */
+  /*------------------------------------------------------------------------------------------------------------*/
+  $requestResponse                                    =   $INTERNAL_SERVER_ERROR;
+  $eMessage                                           =   $e->getMessage();
+  $eLine                                              =   $e->getLine();
+  $eFile                                              =   $e->getFile();
+  $responseMessage                                    =   $resultMessage[$INTERNAL_SERVER_ERROR];
+  $responseMessage                                    =   "$responseMessage...Internal Server Error $eMessage  in File: $eFile in Line: $eLine" ;
+  mysqli_close($mysqli);
+}
+/*------------------------------------------------------------------------------------------------------------*/
+/*                                            Building of Response Object                                     */
+/*------------------------------------------------------------------------------------------------------------*/
+$responseArray=array(
+  'message'                                           =>  strval($responseMessage),
+  'payload'                                           =>  $blockingSegments
+);
+http_response_code($requestResponse);
+echo json_encode($responseArray);
+?>
