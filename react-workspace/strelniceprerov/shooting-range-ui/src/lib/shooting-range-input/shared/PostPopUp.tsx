fa3fcb559037953a32a52581c1bb7a61ac256c31
Diego Claramount <dieg.ruiz@praguelabs.com>
2024-06-28 13:54:09 +0200

    fix: fixing post-popup

--- react-workspace/strelniceprerov/shooting-range-ui/src/lib/shooting-range-input/shared/PostPopUp.tsx
+++ react-workspace/strelniceprerov/shooting-range-ui/src/lib/shooting-range-input/shared/PostPopUp.tsx
@@ -1,6 +1,4 @@
 import Popup from "reactjs-popup";
-import WarningIcon from '@mui/icons-material/Warning';
-import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import Button from '@mui/material/Button';
 import { Typography } from "@mui/material";
 import { useGetEndPoint } from "../ApiCalls/useGetEndPoint";
@@ -8,17 +6,27 @@ import React from "react";
 import { REQUEST_STATUS } from "../ApiCalls/enums";
 import { Spinner } from "./Placeholders";
 import { ManagementDashboardContext } from "../components/Context/ManagementDashboardContext";
-
+import { Translations } from "../types/translations";
+//Icons -> https://fontawesome.com/v4/icons/
 const Banner = (props :any) => {
   return(
     <>
-      {props.status!==200 &&  <WarningIcon style={{marginLeft:'auto', marginRight:'auto', marginTop:'10px', marginBottom:'10px', color:'green'}}/>}
-      {props.status===200 &&  <CheckCircleIcon style={{marginLeft:'auto', marginRight:'auto', marginTop:'10px', marginBottom:'10px', color:'green'}}/>}
-      {props.status!==200 && <Typography sx={{ p: 2 }} style={{ fontWeight:'bold', color:'red', textAlign:'center'}}>{props.payload.message}</Typography>}
-      {props.status===200 && <Typography sx={{ p: 2 }} style={{ fontWeight:'bold', color:'green', textAlign:'center'}}>{props.payload.message}</Typography>}
-      <Button onClick={()=>props.closeModal()} variant='contained' style={{backgroundColor:'green', fontWeight:'bolder', color:'white', width:'auto'}}>
-        Close
-      </Button>
+      {/* REPONSE 200 - OK*/}
+      {props.status===200 &&          <i className="fa fa-check-circle fa-4x"   style={{color:'green', marginLeft:'auto', marginRight:'auto', marginTop:'15px', marginBottom:'15px'}} aria-hidden="true"></i>}
+      {props.status===200 &&          <Typography sx={{ p: 2 }} style={{ height:'100%', marginTop:'auto', marginBottom:'auto', fontWeight:'bold', color:'green', textAlign:'center'}}>{props.payload.message}</Typography>}
+      {props.status===200 &&          <Button onClick={()=>props.closeModal()} variant='contained' style={{marginTop:'auto', backgroundColor:'green', fontWeight:'bolder', color:'white', width:'auto', borderColor:'green'}}>{Translations.PostPoup.CloseButton}</Button>}
+      {/* REPONSE 400 - BAD REQUEST*/}
+      {props.status===400 &&          <i className="fa fa-exclamation-circle fa-4x"   style={{color:'orange',  marginLeft:'auto', marginRight:'auto', marginTop:'15px', marginBottom:'15px'}} aria-hidden="true"></i>}
+      {props.status===400 &&          <Typography sx={{ p: 2 }} style={{height:'100%', marginTop:'auto', marginBottom:'auto', fontWeight:'bold', color:'orange', textAlign:'center'}}>{props.payload.message}</Typography>}
+      {props.status===400 &&          <Button onClick={()=>props.closeModal()} variant='contained' style={{marginTop:'auto', backgroundColor:'orange', fontWeight:'bolder', color:'white', width:'auto', borderColor:'orange'}}>{Translations.PostPoup.CloseButton}</Button>}
+      {/* REPONSE 409 - CONFLICT*/}
+      {props.status===409 &&          <i className="fa fa-exchange fa-4x"   style={{color:'orange',  marginLeft:'auto', marginRight:'auto', marginTop:'15px', marginBottom:'15px'}} aria-hidden="true"></i>}
+      {props.status===409 &&          <Typography sx={{ p: 2 }} style={{height:'100%', marginTop:'auto', marginBottom:'auto', fontWeight:'bold', color:'orange', textAlign:'center'}}>{props.payload.message}</Typography>}
+      {props.status===409 &&          <Button onClick={()=>props.closeModal()} variant='contained' style={{marginTop:'auto', backgroundColor:'orange', fontWeight:'bolder', color:'white', width:'auto', borderColor:'orange'}}>{Translations.PostPoup.CloseButton}</Button>}
+      {/* REPONSE 500 - INTERNAL SERVER ERROR*/}
+      {props.status===500 &&          <i className="fa fa-times-circle fa-5x"   style={{color:'red',  marginLeft:'auto', marginRight:'auto', marginTop:'15px', marginBottom:'15px'}} aria-hidden="true"></i>}
+      {props.status===500 &&          <Typography sx={{ p: 2 }} style={{height:'100%', marginTop:'auto', marginBottom:'auto', fontWeight:'bold', color:'red', textAlign:'center'}}>{props.payload.message}</Typography>}
+      {props.status===500 &&          <Button onClick={()=>props.closeModal()} variant='contained' style={{marginTop:'auto', backgroundColor:'red', fontWeight:'bolder', color:'white', width:'auto', borderColor:'red'}}>{Translations.PostPoup.CloseButton}</Button>}
     </>
   )
 }
--- react-workspace/typescript/shooting-range/src/shooting-range-input/shared/PostPopUp.tsx
+++ react-workspace/typescript/shooting-range/src/shooting-range-input/shared/PostPopUp.tsx
@@ -1,5 +1,4 @@
 import Popup from "reactjs-popup";
-import WarningIcon from '@mui/icons-material/Warning';
 import Button from '@mui/material/Button';
 import { Typography } from "@mui/material";
 import { useGetEndPoint } from "../ApiCalls/useGetEndPoint";
@@ -7,15 +6,26 @@ import React from "react";
 import { ManagementDashboardContext } from "../Context/ManagementDashboardContext";
 import { REQUEST_STATUS } from "../ApiCalls/enums";
 import { Spinner } from "./Placeholders";
-
-const Banner = (payload : any, closeModal: any) => {
+//Icons -> https://fontawesome.com/v4/icons/
+const Banner = (props :any) => {
   return(
     <>
-      <WarningIcon style={{height:'72px', width:'72px', marginLeft:'auto', marginRight:'auto', marginTop:'10px', marginBottom:'10px', color:'green'}}/>
-      <Typography sx={{ p: 2 }} style={{ fontWeight:'bold', color:'black', textAlign:'center'}}>{payload.message}</Typography>
-      <Button onClick={()=>closeModal()} variant='contained' style={{backgroundColor:'green', fontWeight:'bolder', color:'white', width:'auto'}}>
-        Close
-      </Button>
+      {/* REPONSE 200 - OK*/}
+      {props.status===200 &&          <i className="fa fa-check-circle fa-4x"   style={{color:'green', marginLeft:'auto', marginRight:'auto', marginTop:'15px', marginBottom:'15px'}} aria-hidden="true"></i>}
+      {props.status===200 &&          <Typography sx={{ p: 2 }} style={{ height:'100%', marginTop:'auto', marginBottom:'auto', fontWeight:'bold', color:'green', textAlign:'center'}}>{props.payload.message}</Typography>}
+      {props.status===200 &&          <Button onClick={()=>props.closeModal()} variant='contained' style={{marginTop:'auto', backgroundColor:'green', fontWeight:'bolder', color:'white', width:'auto', borderColor:'green'}}>{Translations.PostPoup.CloseButton}</Button>}
+      {/* REPONSE 400 - BAD REQUEST*/}
+      {props.status===400 &&          <i className="fa fa-exclamation-circle fa-4x"   style={{color:'orange',  marginLeft:'auto', marginRight:'auto', marginTop:'15px', marginBottom:'15px'}} aria-hidden="true"></i>}
+      {props.status===400 &&          <Typography sx={{ p: 2 }} style={{height:'100%', marginTop:'auto', marginBottom:'auto', fontWeight:'bold', color:'orange', textAlign:'center'}}>{props.payload.message}</Typography>}
+      {props.status===400 &&          <Button onClick={()=>props.closeModal()} variant='contained' style={{marginTop:'auto', backgroundColor:'orange', fontWeight:'bolder', color:'white', width:'auto', borderColor:'orange'}}>{Translations.PostPoup.CloseButton}</Button>}
+      {/* REPONSE 409 - CONFLICT*/}
+      {props.status===409 &&          <i className="fa fa-exchange fa-4x"   style={{color:'orange',  marginLeft:'auto', marginRight:'auto', marginTop:'15px', marginBottom:'15px'}} aria-hidden="true"></i>}
+      {props.status===409 &&          <Typography sx={{ p: 2 }} style={{height:'100%', marginTop:'auto', marginBottom:'auto', fontWeight:'bold', color:'orange', textAlign:'center'}}>{props.payload.message}</Typography>}
+      {props.status===409 &&          <Button onClick={()=>props.closeModal()} variant='contained' style={{marginTop:'auto', backgroundColor:'orange', fontWeight:'bolder', color:'white', width:'auto', borderColor:'orange'}}>{Translations.PostPoup.CloseButton}</Button>}
+      {/* REPONSE 500 - INTERNAL SERVER ERROR*/}
+      {props.status===500 &&          <i className="fa fa-times-circle fa-5x"   style={{color:'red',  marginLeft:'auto', marginRight:'auto', marginTop:'15px', marginBottom:'15px'}} aria-hidden="true"></i>}
+      {props.status===500 &&          <Typography sx={{ p: 2 }} style={{height:'100%', marginTop:'auto', marginBottom:'auto', fontWeight:'bold', color:'red', textAlign:'center'}}>{props.payload.message}</Typography>}
+      {props.status===500 &&          <Button onClick={()=>props.closeModal()} variant='contained' style={{marginTop:'auto', backgroundColor:'red', fontWeight:'bolder', color:'white', width:'auto', borderColor:'red'}}>{Translations.PostPoup.CloseButton}</Button>}
     </>
   )
 }
@@ -27,8 +37,9 @@ export const PostPopUp = ({postAPI, postParameters, closeModal} : any) => {
   postDone                                  =   postCreationOfSegment.requestStatus  === REQUEST_STATUS.SUCCESS 
   return( <Popup open={true} onClose={()=>closeModal()} closeOnDocumentClick={false} >
             <div style={{backgroundColor:'white', width:'500px', height:'250px', paddingTop:'5px', paddingRight:'15px', paddingLeft:'15px', paddingBottom:'15px', border:'2px solid black', borderRadius:'10px', outline:'10px solid transparent', display:'flex', flexDirection:'column'}}>
-            {postDone ? <Banner payload={postCreationOfSegment.payload} closeModal={closeModal}/> : <Spinner/>}
+            {postDone ? <Banner payload={postCreationOfSegment.payload} status={postCreationOfSegment.status} closeModal={closeModal}/> : <Spinner/>}
             </div>
           </Popup>
     )
 }
+
