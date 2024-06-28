cfeef1eab2159b7df88d59f58db67672de68198b
Diego Claramount <dieg.ruiz@praguelabs.com>
2024-06-28 14:15:05 +0200

    fix: add new placeholders

--- react-workspace/typescript/shooting-range/src/shooting-range-input/shared/Placeholders.tsx
+++ react-workspace/typescript/shooting-range/src/shooting-range-input/shared/Placeholders.tsx
@@ -7,3 +7,21 @@ export function Spinner() {
   </div>
   )
 }
+export function SpinnerPulse() {
+  return(
+    <div style={{width:'100%', height:'100%', display:'flex'}}>
+      <div style={{margin:'auto', width:'auto', height:'auto'}}>
+      <i className="fas fa-spinner fa-pulse" style={{fontSize:'84px'}}></i>
+      </div>
+  </div>
+  )
+}
+export function Arrows() {
+  return(
+    <div style={{width:'100%', height:'100%', display:'flex'}}>
+      <div style={{margin:'auto', width:'auto', height:'auto'}}>
+      <i className="fas fa-sync fa-spin" style={{fontSize:'84px'}}></i>
+      </div>
+  </div>
+  )
+}
