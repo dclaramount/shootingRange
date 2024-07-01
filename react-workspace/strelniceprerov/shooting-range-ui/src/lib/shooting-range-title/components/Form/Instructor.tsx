5fdb24fac9cc00e87fc90638bee1e4ee78abcc8e
Diego Claramount <dieg.ruiz@praguelabs.com>
2024-07-01 15:02:16 +0200

    fix: Fixing the bug for the selected segment (when switching instructors)

--- react-workspace/strelniceprerov/shooting-range-ui/src/lib/shooting-range-title/components/Form/Instructor.tsx
+++ react-workspace/strelniceprerov/shooting-range-ui/src/lib/shooting-range-title/components/Form/Instructor.tsx
@@ -4,18 +4,18 @@ import { BookingContext } from '../Context/BookingContext';
 
 export function Instructor(){
 
-  const {shootingPermit, shootingInstructor, setShootingInstructor} = React.useContext(BookingContext);
+  const {shootingPermit, shootingInstructor, setShootingInstructor,setSelectedSegment} = React.useContext(BookingContext);
 
 
   return(
     <div className="reservation-order-row">
       <label htmlFor="select-box-location">Instruktor</label>
         <div className="reservation-order-row-radios" data-tooltip data-original-title="Instruktor je povinný pro střelce bez ZP.">
-          <input checked={shootingInstructor} onClick={(e) => {setShootingInstructor(true)}} type="radio" name="instructor" id="frm-reservationCalendar-orderForm-instructor-1" value="1" />
+          <input checked={shootingInstructor} onClick={(e) => {setShootingInstructor(true); setSelectedSegment([])}} type="radio" name="instructor" id="frm-reservationCalendar-orderForm-instructor-1" value="1" />
             <label htmlFor="frm-reservationCalendar-orderForm-instructor-1">
               Ano
             </label>
-            <input checked={!shootingInstructor} onClick={(e) => {if(shootingPermit){setShootingInstructor(false)}}} type="radio" name="instructor" id="frm-reservationCalendar-orderForm-instructor-0" value="0" />
+            <input checked={!shootingInstructor} onClick={(e) => {if(shootingPermit){setShootingInstructor(false); setSelectedSegment([])}}} type="radio" name="instructor" id="frm-reservationCalendar-orderForm-instructor-0" value="0" />
             <label htmlFor="frm-reservationCalendar-orderForm-instructor-0">
               Ne
             </label>
--- react-workspace/strelniceprerov/shooting-range-ui/src/lib/shooting-range-title/components/Form/ShootingPermit.tsx
+++ react-workspace/strelniceprerov/shooting-range-ui/src/lib/shooting-range-title/components/Form/ShootingPermit.tsx
@@ -5,7 +5,7 @@ import Tooltip from '@mui/material/Tooltip';
 
 export function ShootingPermit(){
 
-  const {shootingPermit, setShootingPermit, shootingPermitNumber, setShootingPermitNumber, setShootingInstructor} = React.useContext(BookingContext);
+  const {shootingPermit, setShootingPermit, shootingPermitNumber, setShootingPermitNumber, setShootingInstructor, setSelectedSegment} = React.useContext(BookingContext);
 
   const handleChangePermit = (e:any) => {
     setShootingPermitNumber(e.target.value);
@@ -15,11 +15,11 @@ export function ShootingPermit(){
     <div className="reservation-order-row">
       <label htmlFor="select-box-location">Zbrojní průkaz</label>
       <div className="reservation-order-row-radios">
-        <input checked={shootingPermit} onClick={(e) => {setShootingPermit(true)}} type="radio" name="licence" id="frm-reservationCalendar-orderForm-licence-1" required={true} data-nette-rules="[{&quot;op&quot;:&quot;:filled&quot;,&quot;msg&quot;:&quot;Prosím vyberte, jestli vlastníte zbrojní průkaz. Střelci bez zbrojního průkazu k sobě musí mít instruktora.&quot;}]" value="1"/>
+        <input checked={shootingPermit} onClick={(e) => {setShootingPermit(true); setSelectedSegment([])}} type="radio" name="licence" id="frm-reservationCalendar-orderForm-licence-1" required={true} data-nette-rules="[{&quot;op&quot;:&quot;:filled&quot;,&quot;msg&quot;:&quot;Prosím vyberte, jestli vlastníte zbrojní průkaz. Střelci bez zbrojního průkazu k sobě musí mít instruktora.&quot;}]" value="1"/>
         <label htmlFor="frm-reservationCalendar-orderForm-licence-1">
           Ano
         </label>
-        <input checked={!shootingPermit} onClick={(e) => {setShootingPermit(false);setShootingInstructor(true);}} type="radio" name="licence" id="frm-reservationCalendar-orderForm-licence-0" required={true} data-nette-rules="[{&quot;op&quot;:&quot;:filled&quot;,&quot;msg&quot;:&quot;Prosím vyberte, jestli vlastníte zbrojní průkaz. Střelci bez zbrojního průkazu k sobě musí mít instruktora.&quot;}]" value="0"/>
+        <input checked={!shootingPermit} onClick={(e) => {setShootingPermit(false);setShootingInstructor(true); setSelectedSegment([])}} type="radio" name="licence" id="frm-reservationCalendar-orderForm-licence-0" required={true} data-nette-rules="[{&quot;op&quot;:&quot;:filled&quot;,&quot;msg&quot;:&quot;Prosím vyberte, jestli vlastníte zbrojní průkaz. Střelci bez zbrojního průkazu k sobě musí mít instruktora.&quot;}]" value="0"/>
         <label htmlFor="frm-reservationCalendar-orderForm-licence-0">
           Ne
         </label>
