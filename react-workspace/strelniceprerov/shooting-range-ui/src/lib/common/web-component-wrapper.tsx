2098fd4e8798379b950133a646a01156f87849a1
Diego Claramount <dieg.ruiz@praguelabs.com>
2024-07-31 08:35:09 +0200

    fix: forcing to always fetch css

--- react-workspace/strelniceprerov/shooting-range-ui/src/lib/common/web-component-wrapper.tsx
+++ react-workspace/strelniceprerov/shooting-range-ui/src/lib/common/web-component-wrapper.tsx
@@ -36,97 +36,97 @@ export function WebComponentWrapper(
 
       const linkElem = document.createElement('link');
       linkElem.setAttribute('rel', 'stylesheet');
-      linkElem.setAttribute('href', 'https://strelniceprerov.cz/wp-admin/css/bw-bootstrap-strelnice.css');
+      linkElem.setAttribute('href', `https://strelniceprerov.cz/wp-admin/css/bw-bootstrap-strelnice.css?dummy=${new Date()}`);
 
       const linkElem2 = document.createElement('link');
       linkElem2.setAttribute('rel', 'stylesheet');
-      linkElem2.setAttribute('href', 'https://strelniceprerov.cz/wp-admin/css/bw-calendar.css');
+      linkElem2.setAttribute('href', `https://strelniceprerov.cz/wp-admin/css/bw-calendar.css?dummy=${new Date()}`);
 
       const linkElem3 = document.createElement('link');
       linkElem3.setAttribute('rel', 'stylesheet');
-      linkElem3.setAttribute('href', 'https://strelniceprerov.cz/wp-admin/css/bw-front-strelnice.css');
+      linkElem3.setAttribute('href', `https://strelniceprerov.cz/wp-admin/css/bw-front-strelnice.css?dummy=${new Date()}`);
 
       const linkElem4 = document.createElement('link');
       linkElem4.setAttribute('rel', 'stylesheet');
-      linkElem4.setAttribute('href', 'https://strelniceprerov.cz/wp-admin/css/bw-print-strelnice.css');
+      linkElem4.setAttribute('href', `https://strelniceprerov.cz/wp-admin/css/bw-print-strelnice.css?dummy=${new Date()}`);
 
       const linkElem5 = document.createElement('link');
       linkElem5.setAttribute('rel', 'stylesheet');
-      linkElem5.setAttribute('href', 'https://strelniceprerov.cz/wp-admin/css/bw-reboot-strelnice.css');
+      linkElem5.setAttribute('href', `https://strelniceprerov.cz/wp-admin/css/bw-reboot-strelnice.css?dummy=${new Date()}`);
 
       const linkElem6 = document.createElement('link');
       linkElem6.setAttribute('rel', 'stylesheet');
-      linkElem6.setAttribute('href', 'https://strelniceprerov.cz/wp-admin/css/bw-reservation-strelnice.css');
+      linkElem6.setAttribute('href', `https://strelniceprerov.cz/wp-admin/css/bw-reservation-strelnice.css?dummy=${new Date()}`);
 
       const linkElem7 = document.createElement('link');
       linkElem7.setAttribute('rel', 'stylesheet');
-      linkElem7.setAttribute('href', 'https://strelniceprerov.cz/wp-admin/css/bw-honest-week-picker.css');
+      linkElem7.setAttribute('href', `https://strelniceprerov.cz/wp-admin/css/bw-honest-week-picker.css?dummy=${new Date()}`);
 
       const linkElem8 = document.createElement('link');
       linkElem8.setAttribute('rel', 'stylesheet');
-      linkElem8.setAttribute('href', "//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css");
+      linkElem8.setAttribute('href', `//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css?dummy=${new Date()}`);
       
       //DEV EXTREME
       const linkElem9 = document.createElement('link');
       linkElem9.setAttribute('rel', 'stylesheet');
-      linkElem9.setAttribute('href', 'https://strelniceprerov.cz/wp-admin/css/bw-dx-demo-template.css');
+      linkElem9.setAttribute('href', `https://strelniceprerov.cz/wp-admin/css/bw-dx-demo-template.css?dummy=${new Date()}`);
 
       const linkElem10 = document.createElement('link');
       linkElem10.setAttribute('rel', 'stylesheet');
-      linkElem10.setAttribute('href', 'https://strelniceprerov.cz/wp-admin/css/bw-dx-light.css');
+      linkElem10.setAttribute('href', `https://strelniceprerov.cz/wp-admin/css/bw-dx-light.css?dummy=${new Date()}`);
 
       const linkElem11 = document.createElement('link');
       linkElem11.setAttribute('rel', 'stylesheet');
-      linkElem11.setAttribute('href', 'https://strelniceprerov.cz/wp-admin/css/bw-dx-material-blue-light.css');
+      linkElem11.setAttribute('href', `https://strelniceprerov.cz/wp-admin/css/bw-dx-material-blue-light.css?dummy=${new Date()}`);
 
       const linkElem12 = document.createElement('link');
       linkElem12.setAttribute('rel', 'stylesheet');
-      linkElem12.setAttribute('href', 'https://strelniceprerov.cz/wp-admin/css/bw-calendar-instructors.css');
+      linkElem12.setAttribute('href', `https://strelniceprerov.cz/wp-admin/css/bw-calendar-instructors.css?dummy=${new Date()}`);
 
       //CSS files for the plugin of calendar
       const linkElem13 = document.createElement('link');
       linkElem13.setAttribute('rel', 'stylesheet');
-      linkElem13.setAttribute('href', 'https://strelniceprerov.cz/wp-admin/css/ej2_react_schedule_material.css');
+      linkElem13.setAttribute('href', `https://strelniceprerov.cz/wp-admin/css/ej2_react_schedule_material.css?dummy=${new Date()}`);
 
       const linkElem14 = document.createElement('link');
       linkElem14.setAttribute('rel', 'stylesheet');
-      linkElem14.setAttribute('href', 'https://strelniceprerov.cz/wp-admin/css/ej2_splitbuttons_material.css');
+      linkElem14.setAttribute('href', `https://strelniceprerov.cz/wp-admin/css/ej2_splitbuttons_material.css?dummy=${new Date()}`);
   
       const linkElem15 = document.createElement('link');
       linkElem15.setAttribute('rel', 'stylesheet');
-      linkElem15.setAttribute('href', 'https://strelniceprerov.cz/wp-admin/css/ej2_popups_material.css');
+      linkElem15.setAttribute('href', `https://strelniceprerov.cz/wp-admin/css/ej2_popups_material.css?dummy=${new Date()}`);
 
       const linkElem16 = document.createElement('link');
       linkElem16.setAttribute('rel', 'stylesheet');
-      linkElem16.setAttribute('href', 'https://strelniceprerov.cz/wp-admin/css/ej2_navigations_material.css');
+      linkElem16.setAttribute('href', `https://strelniceprerov.cz/wp-admin/css/ej2_navigations_material.css?dummy=${new Date()}`);
 
       const linkElem17 = document.createElement('link');
       linkElem17.setAttribute('rel', 'stylesheet');
-      linkElem17.setAttribute('href', 'https://strelniceprerov.cz/wp-admin/css/ej2_lists_material.css');
+      linkElem17.setAttribute('href', `https://strelniceprerov.cz/wp-admin/css/ej2_lists_material.css?dummy=${new Date()}`);
 
       const linkElem18 = document.createElement('link');
       linkElem18.setAttribute('rel', 'stylesheet');
-      linkElem18.setAttribute('href', 'https://strelniceprerov.cz/wp-admin/css/ej2_inputs_material.css');
+      linkElem18.setAttribute('href', `https://strelniceprerov.cz/wp-admin/css/ej2_inputs_material.css?dummy=${new Date()}`);
 
       const linkElem19 = document.createElement('link');
       linkElem19.setAttribute('rel', 'stylesheet');
-      linkElem19.setAttribute('href', 'https://strelniceprerov.cz/wp-admin/css/ej2_dropdowns_material.css');
+      linkElem19.setAttribute('href', `https://strelniceprerov.cz/wp-admin/css/ej2_dropdowns_material.css?dummy=${new Date()}`);
 
       const linkElem20 = document.createElement('link');
       linkElem20.setAttribute('rel', 'stylesheet');
-      linkElem20.setAttribute('href', 'https://strelniceprerov.cz/wp-admin/css/ej2_calendars_material.css');
+      linkElem20.setAttribute('href', `https://strelniceprerov.cz/wp-admin/css/ej2_calendars_material.css?dummy=${new Date()}`);
 
       const linkElem21 = document.createElement('link');
       linkElem21.setAttribute('rel', 'stylesheet');
-      linkElem21.setAttribute('href', 'https://strelniceprerov.cz/wp-admin/css/ej2_buttons_material.css');
+      linkElem21.setAttribute('href', `https://strelniceprerov.cz/wp-admin/css/ej2_buttons_material.css?dummy=${new Date()}`);
 
       const linkElem22 = document.createElement('link');
       linkElem22.setAttribute('rel', 'stylesheet');
-      linkElem22.setAttribute('href', 'https://strelniceprerov.cz/wp-admin/css/ej2_base_material.css');
+      linkElem22.setAttribute('href', `https://strelniceprerov.cz/wp-admin/css/ej2_base_material.css?dummy=${new Date()}`);
 
       const linkElem23 = document.createElement('link');
       linkElem23.setAttribute('rel', 'stylesheet');
-      linkElem23.setAttribute('href', 'https://strelniceprerov.cz/wp-admin/css/smart.default.css');
+      linkElem23.setAttribute('href', `https://strelniceprerov.cz/wp-admin/css/smart.default.css?dummy=${new Date()}`);
 
       mountPoint.id = NAME;
       shadowRoot.appendChild(mountPoint);
