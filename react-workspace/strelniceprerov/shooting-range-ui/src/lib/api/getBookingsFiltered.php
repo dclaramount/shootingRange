514066c222e0bb5c84406f13423ae4d77ed9e502
Diego Claramount <diegoaclaramount@gmail.com>
2024-12-18 14:36:21 +0100

    fix: error in sql query for this api endpoint.

--- react-workspace/strelniceprerov/shooting-range-ui/src/lib/api/getBookingsFiltered.php
+++ react-workspace/strelniceprerov/shooting-range-ui/src/lib/api/getBookingsFiltered.php
@@ -13,17 +13,17 @@ $mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $d
 //Variable that will hold the response
 $responseArray = array();
 
-if (isset($_GET['firstDayOfWeek']) && $_GET['firstDayOfWeek']!="") {
+if (isset($_GET['firstDayOfWeek']) && $_GET['firstDayOfWeek'] != "") {
   $firstDay = $_GET['firstDayOfWeek'];
 } else {
   $firstDay = "";
 }
-if (isset($_GET['lastDayOfWeek']) && $_GET['lastDayOfWeek']!="") {
+if (isset($_GET['lastDayOfWeek']) && $_GET['lastDayOfWeek'] != "") {
   $lastDay = $_GET['lastDayOfWeek'];
 } else {
   $lastDay = "";
 }
-if (isset($_GET['location']) && $_GET['location']!="") {
+if (isset($_GET['location']) && $_GET['location'] != "") {
   $locationId = $_GET['location'];
 } else {
   $locationId = "";
@@ -34,28 +34,29 @@ if (isset($_GET['location']) && $_GET['location']!="") {
 $query = "SELECT  location.name as 'Location',
                   bookings.invoice_id as 'Invoice',
                   bookings.number_of_people as 'Occupancy',
-                  location.capacity as 'MaxCapacity',
+                  svc.max_capacity as 'MaxCapacity',
                   bookings.number_of_hours as 'Length',
                   bookings.start_time as 'Start',
                   bookings.end_time as 'End' FROM  invoice_item as bookings
                   INNER JOIN location as location ON bookings.location_id=location.id
+                  INNER JOIN services as svc ON svc.location_id=location.id
                   WHERE bookings.start_time >= '$firstDay'  AND bookings.end_time <= '$lastDay' AND bookings.location_id='$locationId';";
- 
+
 //Retrieving the records
-$res = mysqli_query($mysqli, $query, MYSQLI_USE_RESULT) or die( mysqli_error($mysqli));
+$res = mysqli_query($mysqli, $query, MYSQLI_USE_RESULT) or die(mysqli_error($mysqli));
 if ($res) {
   while ($row = mysqli_fetch_row($res)) {
-    $responseArray[]=array( 'location'        => $row[0], 
-                            'invoiceId'       => $row[1],
-                            'occupancy'       => $row[2],
-                            'maxOccupancy'    => $row[3],
-                            'length'          => $row[4],
-                            'start'           => $row[5],
-                            'end'             => $row[6]);
-                          
+    $responseArray[] = array(
+      'location'        => $row[0],
+      'invoiceId'       => $row[1],
+      'occupancy'       => $row[2],
+      'maxOccupancy'    => $row[3],
+      'length'          => $row[4],
+      'start'           => $row[5],
+      'end'             => $row[6]
+    );
   }
   $res->free_result();
 }
 mysqli_close($mysqli);
 echo json_encode($responseArray);
-?>
